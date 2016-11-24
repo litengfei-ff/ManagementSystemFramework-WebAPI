c5c6afa943648a9612011802d9f0647d12c85543
ff <o_litengfei@163.com>
2016-11-24 14:12:50 +0800

    测试 Git

--- src/FoundationFramework/Controllers/GetTokenController.cs
+++ src/FoundationFramework/Controllers/GetTokenController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
-
+ 
 
 namespace FoundationFramework.Controllers
 {
